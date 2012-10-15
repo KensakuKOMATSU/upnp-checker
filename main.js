52ded163cd74d302e4e88da6c64765acdbdb4500
kensakukomatsu <kensaku.komatsu@gmail.com>
2012-10-16 00:55:54 +0900

    implement console mode checker

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 var upnp = new UPnP();
+upnp.onready = function(){
+	upnp.listen(function(e){console.dir(e);})
+
+	console.log("type upnp.search(\"ssdp:all\"); or something (parameter is ST), you'll find UPnP devices.")
+}
